7e7f96d5fccf4fe016f383d9f1f4b0a9f2dce567
IDboo <topcu275@gmail.com>
2024-03-26 18:09:58 +0300

    .

--- HotelGame.WebMVC/wwwroot/js/site.js
+++ HotelGame.WebMVC/wwwroot/js/site.js
@@ -1,4 +1,12 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var ifLogged = true;
 
-// Write your JavaScript code.
+function sw() {
+    console.log(ifLogged);
+    ifLogged = !ifLogged;
+
+    if (ifLogged) {
+        document.getElementById('loginButtonText').innerHTML = 'Oyna';
+    } else {
+        document.getElementById('loginButtonText').innerHTML = 'Devam Et';
+    }
+}
