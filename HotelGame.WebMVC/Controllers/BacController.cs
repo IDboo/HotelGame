643c3a9db4d0e72f27494a3d91b499b3d71aeaed
bakihz <cmylmzfake@gmail.com>
2024-04-03 11:42:40 +0300

    new page

---
+++ HotelGame.WebMVC/Controllers/BacController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelGame.WebMVC.Controllers
+{
+    public class BacController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
