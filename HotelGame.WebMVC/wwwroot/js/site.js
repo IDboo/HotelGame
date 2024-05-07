ee629582bdc1187024e6201bfd1bf481a6324965
IDboo <topcu275@gmail.com>
2024-05-07 03:08:52 +0300

    ana sayfa

--- HotelGame.WebMVC/wwwroot/js/site.js
+++ HotelGame.WebMVC/wwwroot/js/site.js
@@ -1,20 +1,35 @@
-﻿var ifLogged = true;
-
-function sw() {
-    console.log(ifLogged);
-    ifLogged = !ifLogged;
-
-    if (ifLogged) {
-        document.getElementById('loginButtonText').innerHTML = 'Oyna';
-    } else {
-        document.getElementById('loginButtonText').innerHTML = 'Devam Et';
-    }
-}
-
-function openLoginPopup() {
+﻿function openLoginPopup() {
+    document.getElementById("SigninPopup").style.display = "none";
     document.getElementById("loginPopup").style.display = "block";
 }
 
 function closeLoginPopup() {
     document.getElementById("loginPopup").style.display = "none";
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.addEventListener('click', function (event) {
+        var popup = document.getElementById('loginPopup');
+        if (!popup.contains(event.target) && event.target.id !== 'loginButton') {
+            popup.style.display = "none";
+        }
+    });
+});
+
+function openSigninPopup() {
+    document.getElementById("loginPopup").style.display = "none";
+    document.getElementById("SigninPopup").style.display = "block";
+}
+
+function closeSigninPopup() {
+    document.getElementById("SigninPopup").style.display = "none";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.addEventListener('click', function (event) {
+        var popup = document.getElementById('SigninPopup');
+        if (!popup.contains(event.target) && event.target.id !== 'signinButton') {
+            popup.style.display = "none";
+        }
+    });
+});
