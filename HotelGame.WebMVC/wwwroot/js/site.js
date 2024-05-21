87dc6be6f0e98b728b792e3459260f07073dce92
IDboo <topcu275@gmail.com>
2024-05-21 18:00:29 +0300

    1

--- HotelGame.WebMVC/wwwroot/js/site.js
+++ HotelGame.WebMVC/wwwroot/js/site.js
@@ -10,7 +10,7 @@ function closeLoginPopup() {
 document.addEventListener("DOMContentLoaded", function () {
     document.addEventListener('click', function (event) {
         var popup = document.getElementById('loginPopup');
-        if (!popup.contains(event.target) && event.target.id !== 'loginButton') {
+        if (!popup.contains(event.target) && event.target.id !== 'loginButton' && event.target.id !== 'bellButton') {
             popup.style.display = "none";
         }
     });
@@ -33,3 +33,25 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+function handleBellClick() {
+    const bellButton = document.getElementById('bellButton');
+    const normalBellSrc = '/img/Çizgisiz Zil.png';
+    const pressedBellSrc = '/img/Çizgili ve Basık Zil.png';
+    const ringingBellSrc = '/img/Zil.png';
+
+    bellButton.src = pressedBellSrc;
+    bellButton.classList.add('pressed');
+
+    setTimeout(() => {
+        bellButton.src = ringingBellSrc;
+        bellButton.classList.remove('pressed');
+    }, 100); // 100ms sonra basılı zil görüntüsü yerine ses çıkaran zil görüntüsü
+
+    openLoginPopup();
+
+    setTimeout(() => {
+        bellButton.src = normalBellSrc;
+    }, 500); // 500ms sonra tekrar normal zil görüntüsüne dön
+
+}
