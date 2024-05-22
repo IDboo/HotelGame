3857265673140b93799e0c3f09e0536e88f6ebb7
IDboo <topcu275@gmail.com>
2024-05-23 01:21:33 +0300

    1

--- HotelGame.WebMVC/wwwroot/user/js/userMain.js
+++ HotelGame.WebMVC/wwwroot/user/js/userMain.js
@@ -1,1 +1,9 @@
-﻿
+﻿<script>
+    document.addEventListener("DOMContentLoaded", function() {
+    var gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach(function(item, index) {
+        // Burada her öğeye ek işlemler yapabilirsiniz.
+        console.log('Item ID:', item.getAttribute('data-room-id'), 'Class Name:', 'grid-item-' + index);
+    });
+});
+</script>
