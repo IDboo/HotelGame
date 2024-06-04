63bdc0b3e14db2e050c8764f438e555a6499b608
IDboo <topcu275@gmail.com>
2024-06-04 16:26:33 +0300

    1

---
+++ HotelGame.WebMVC/Models/Users/UserAddViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelGame.WebMVC.Models.Users
+{
+    public class UserAddViewModel : BaseViewModel
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string[] Roles { get; set; }
+
+    }
+}
---
+++ HotelGame.WebMVC/Models/Users/UserUpdateViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelGame.WebMVC.Models.Users
+{
+    public class UserUpdateViewModel : BaseViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string[] Roles { get; set; }
+    }
+}
